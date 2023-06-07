e83f8d0f21c1cd2a374598b692ba86160ad39216
mohammadjalizi <mjalizi1376@gmail.com>
2023-06-07 08:07:58 +0330

    added folder backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,36 @@
 
-import './App.css';
+import './App.css'
+
+import React from 'react';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './assaste/Root';
+import Header from './assaste/Header/Header';
+import Store from './assaste/store/Store';
 
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+ 
+      <Route path="/home" element={<Header />} />
+     
+      <Route path="/store" element={<Store />} />
+    </Route>
+  )
+);
 
 
 
 function App() {
   return (
     <div className="App">
+   <RouterProvider router={router} />
 
     </div>
   );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux'
-import { store } from './redux/Store'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './assaste/Root';
-import Header from './assaste/Header/Header';
-import Store from './assaste/store/Store';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route path="/home" element={<Header />} />
-      <Route path="/store" element={<Store />} />
-      {/* ... etc. */}
-    </Route>
-  )
-  );
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
 
-     <RouterProvider router={router} />   
-     </Provider>
+
+ <App/>
+
 
   </React.StrictMode>
 );
