edbcd800908cacf5ba2d30abc70dc44d35a2c3a7
mohammadjalizi <mjalizi1376@gmail.com>
2023-06-08 22:22:14 +0330

    using redux tolkit

--- front-end/src/redux/MyData.js
+++ front-end/src/redux/MyData.js
@@ -10,7 +10,14 @@ const initialState = {
         price: 100,
         imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/1_mrh258.jpg",
       },
-
+      {
+        id: 1,
+        productName: "T-shirt",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+        price: 100,
+        imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1680872470/online%20store/1_mrh258.jpg",
+      },
  ]
 
 
