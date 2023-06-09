147582e3e73da30db95cec8a6c0ab63daa71b98e
mohammadjalizi <mjalizi1376@gmail.com>
2023-06-09 08:03:54 +0330

    addToCartFUCNCTION

--- front-end/src/assaste/Navbar/Navbar.jsx
+++ front-end/src/assaste/Navbar/Navbar.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 
 import './Navbar.css'
+import { useDispatch, useSelector } from 'react-redux';
+
+
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const {selectorproducts} = useSelector((state) => state.counter)
+
   return (
     <div className=' w-full flex justify-between py-4  bg-teal-800 text-black  '>
 <div className=' flex '> 
@@ -27,7 +32,9 @@ navigate('/home')
 
 navigate('/cart')
 
- }}  className=' text-2xl cursor-pointer hover:text-cyan-300 '>cart</h2>  
+ }}  className=' text-2xl cursor-pointer hover:text-cyan-300 '>cart
+ <span className='  border shadow-2xl  ml-4'>{selectorproducts.length}</span>
+ </h2>  
  <h1>mohammadjal</h1>    
 
 
