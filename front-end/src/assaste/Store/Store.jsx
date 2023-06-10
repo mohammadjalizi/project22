276475058caf8f0fc5175b815a8de339c99dc437
mohammadjalizi <mjalizi1376@gmail.com>
2023-06-10 17:55:49 +0330

    delete item for array

--- front-end/src/assaste/Store/Store.jsx
+++ front-end/src/assaste/Store/Store.jsx
@@ -4,7 +4,7 @@ import './Srote.css'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { useGetproductsByNameQuery } from '../../redux/ProdutsApi'
-import { AddtoCart } from 'redux/MyData'
+import { AddtoCart, decreaseProduct, deletproduct, increaseProduct } from 'redux/MyData'
 
 
 const Store = () => {
@@ -31,40 +31,38 @@ return(
       <div className=' flex flex-wrap   gap-9  justify-center    '>
   
   
-  {data.map((item) => {
+  {data.map((item,index) => {
           return (
             <div key={item.id} className=' Card shadow-2x     w-96  text-center my-12   '>
             <h1>{item.productName}</h1>
             <img src={item.imageLink} alt="" />
 
             <div className='  pb-3 mt-14 flex justify-between'>
-             {selectorproductsMyid.includes(item.id)?(
-  <button onClick={() => {
-    dispatch(AddtoCart(item))
-  }
-  }>AddtoCart</button>
-             ) : (
-
+             {false?(
 <>
-<button   onClick={() => {
-  dispatch(increaseProduct(item))
+<button onClick={() => {
+  dispatch(AddtoCart(item))
 }
-} className= ' m-10  text-3xl '> +</button>
-
-<h1 onClick={() => {
-  dispatch(deletproduct(item))
-}
-}>delet</h1>
-
-<h1> {item.quntity}  </h1>
-<button onClick={(() => {
-  dispatch(decreaseProduct(item))
-}
-)} className='text-3xl'>-</button>
-
+}>  addtocart  </button>
 </>
+             ) :  (
+           <>     <button   onClick={() => {
+            dispatch(increaseProduct(item))
+          }
+          } className= ' m-10  text-3xl '> +</button>
+          
+          <h1 onClick={() => {
+            dispatch(deletproduct(item))
+          }
+          }>delet</h1>
+          
+          <h1> 12  </h1>
+          <button onClick={(() => {
+            dispatch(decreaseProduct(item))
+          }
+          )} className='text-3xl'>-</button>  </>
+             ) }
 
-             )  }
 
             
 <p>   {item.price}   </p>
